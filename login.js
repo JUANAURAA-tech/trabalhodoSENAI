b8be858a711240c25208d70b7a38ebeb2e86ea2b
JUANAURAA-tech <juanpablogomes884@gmail.com>
2026-09-01 14:45:42 -0400

    Adicionando arquivos do projeto

---
+++ login.js
@@ -0,0 +1,22 @@
+function login() {
+    let email = document.getElementById('email').value;
+    let senha = document.getElementById('senha').value;
+
+    // REGEX
+    let regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let regexSenha = /^\d{6}$/;
+
+    if (
+        regexEmail.test(email) &&
+        regexSenha.test(senha) &&
+        (
+            (email === "Adm@gmail.com" && senha === "654321") ||
+            (email === "josepher@gmail.com" && senha === "123456")
+        )
+    ) {
+        alert("Login realizado com sucesso!");
+        window.location.href = "file:///C:/Users/SENAI/Desktop/SolusGrafEpa/solusgrafepa.html";
+    } else {
+        alert("Email ou senha incorretos.");
+    }
+}
